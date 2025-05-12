030a6cf9a5e81e88fed20448d3a102b9e3d8f05b
nadatecoco <yuutoshimodate@gmail.com>
2025-05-12 21:26:56 +0900

    Fix: Add missing migrations, tests and fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
-  get 'items/index'
   devise_for :users
-  root to: 'items#index' 
+  root to: 'items#index'
   resources :items
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
