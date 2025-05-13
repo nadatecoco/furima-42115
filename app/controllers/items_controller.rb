c83427bdf09d20558f80fdb59bc84bccadccafd1
nadatecoco <yuutoshimodate@gmail.com>
2025-05-13 14:53:16 +0900

    指摘修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,6 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      Rails.logger.debug "Validation errors: #{@item.errors.full_messages}"
       render :new, status: :unprocessable_entity
     end
   end
