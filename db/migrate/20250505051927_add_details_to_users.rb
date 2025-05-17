4aa5b9701dbd7bfb711a7411932b0fd364ef04c5
nadatecoco <yuutoshimodate@gmail.com>
2025-05-17 15:32:01 +0900

    不要なマイグレーション削除

--- db/migrate/20250505051927_add_details_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :last_name, :string, null: false
-    add_column :users, :first_name, :string, null: false
-    add_column :users, :kana_last_name, :string, null: false
-    add_column :users, :kana_first_name, :string, null: false
-    add_column :users, :birthday, :date, null: false
-  end
-end
