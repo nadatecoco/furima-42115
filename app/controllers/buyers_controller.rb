a3fa2966c3495f40fe82bd0d70969ae649da184c
nadatecoco <yuutoshimodate@gmail.com>
2025-05-26 16:42:49 +0900

    決済機能実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,6 +1,6 @@
 class BuyersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: [:index, :create, :show]
+  before_action :set_item, only: [:index, :create]
 
   def index
     @buyer_address = BuyerAddress.new
@@ -11,6 +11,7 @@ class BuyersController < ApplicationController
     if @buyer_address.valid?
       pay_item
       @buyer_address.save
+      redirect_to root_path
     else
       render :index, status: :unprocessable_entity
     end
@@ -28,5 +29,14 @@ class BuyersController < ApplicationController
   def buyer_address_params
     params.permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
           .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency: 'jpy'
+    )
   end
 end
