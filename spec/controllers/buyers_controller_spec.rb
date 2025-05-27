ee1c70bf5b22bbf2c670fcaa37f79f7b089f407d
nadatecoco <yuutoshimodate@gmail.com>
2025-05-27 14:40:08 +0900

    スペルミス

--- spec/controllers/buyers_controller_spec.rb
+++ spec/controllers/buyers_controller_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe BuyersController, type: :controller do
           postal_code: '123-4567',
           prefecture_id: 2,
           city: '横浜市',
-          addresses: '1-1-1',
+          address: '1-1-1',
           building: 'テストビル',
           phone_number: '09012345678'
         },
