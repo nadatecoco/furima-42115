929e85f3f7992423b4d540458d421645d33c2491
nadatecoco <yuutoshimodate@gmail.com>
2025-05-31 20:28:26 +0900

    外部キーをファクトリーボットに作成

--- spec/models/buyer_address_spec.rb
+++ spec/models/buyer_address_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe BuyerAddress, type: :model do
   describe '配送先住所のバリデーション' do
     before do
-      @buyer_address = build(:buyer_address)
+      @user = create(:user)
+      @item = create(:item)
+      @buyer_address = build(:buyer_address, user_id: @user.id, item_id: @item.id)
     end
 
     context '正常系' do
